b0ac3d9bd5b932c4d533f1da08e81c5a061eeba8
egasmaaeldaly-88 <eg.asmaaeldaly@gmail.com>
2026-06-04 23:07:23 +0300

    update silver patient.sql file

--- healthcare_dbt/models/silver/silver_patients.sql
+++ healthcare_dbt/models/silver/silver_patients.sql
@@ -22,24 +22,26 @@ deduped AS (
 cleaned AS (
     SELECT
         patient_id,
-        INITCAP(first_name)                                     AS first_name,
-        INITCAP(last_name)                                      AS last_name,
-        CONCAT(INITCAP(first_name), ' ', INITCAP(last_name))   AS full_name,
-        date_of_birth,
-        DATEDIFF(current_date(), date_of_birth) / 365           AS age_years,
+        -- أضيفي هذا السطر هنا:
+        COALESCE(national_id, 'UNKNOWN_ID') AS national_id,
         
-        -- توحيد وتنظيف قيم الجنس لتتوافق مع معايير الـ Silver والاختبارات
+        INITCAP(first_name)                                   AS first_name,
+        INITCAP(last_name)                                    AS last_name,
+        CONCAT(INITCAP(first_name), ' ', INITCAP(last_name))  AS full_name,
+        date_of_birth,
+        DATEDIFF(current_date(), date_of_birth) / 365         AS age_years,
+        
         CASE 
             WHEN UPPER(TRIM(gender)) = 'MALE' THEN 'M'
             WHEN UPPER(TRIM(gender)) = 'FEMALE' THEN 'F'
             ELSE 'UNSPECIFIED'
-        END                                                     AS gender,
+        END                                                   AS gender,
         
         blood_type,
-        LOWER(contact_email)                                    AS contact_email,
+        LOWER(contact_email)                                  AS contact_email,
         created_at,
         _bronze_loaded_at,
-        current_timestamp()                                     AS _silver_loaded_at
+        current_timestamp()                                   AS _silver_loaded_at
     FROM deduped
     WHERE rn = 1
 )
